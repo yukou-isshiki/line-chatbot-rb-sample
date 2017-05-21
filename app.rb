b5097df4b74e8ae5381213d32df82802ba95ef48
一色優孝 <isshikiyuukou@isshikiyuukounoMacBook-Air.local>
2017-05-21 19:30:54 +0900

    azure api add

--- app.rb
+++ app.rb
@@ -86,3 +86,23 @@ post '/callback' do
 
   "OK"
 end
+
+require 'net/http'
+
+uri = URI('https://westus.api.cognitive.microsoft.com/luis/api/v2.0/apps/')
+uri.query = URI.encode_www_form({
+})
+
+request = Net::HTTP::Post.new(uri.request_uri)
+# Request headers
+request['Content-Type'] = 'application/json'
+# Request headers
+request['Ocp-Apim-Subscription-Key'] = '{subscription key}'
+# Request body
+request.body = "{body}"
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(request)
+end
+
+puts response.body
